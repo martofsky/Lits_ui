817d7483cb7a2fd2da1ea2d4bb5240df611ef648
martofsky <martofsky.r.j@gmail.com>
2017-04-14 16:06:43 +0300

    upd

--- homework/js/cars.js
+++ homework/js/cars.js
@@ -2,12 +2,33 @@ var Car = function(num_of_wheels, speed) {
     this.num_of_wheels = num_of_wheels || '--';
     this.speed = speed || '--';
 }
-Car.prototype.method_drive = function() {
-        return console.log('Driving on ' + this.speed + 'kms per hour');
-    }
-var truck = new Car(6);
-var bycicle = new Car(2, 20);
+Car.prototype.drive = function() {
+        return console.log('Driving on car' + this.speed + 'kms per hour');
+}
 
+var Truck = function(num_of_wheels, speed) { 
+ Car.call(this, num_of_wheels, speed); 
+} 
+Truck.prototype = Object.create(Car.prototype);
+Truck.prototype.drive = function () {
+ console.log('Driving on truck' + this.speed + 'kms per hour');
+}
+
+var Bycicle = function(num_of_wheels, speed) { 
+ Car.call(this, num_of_wheels, speed); 
+ this.num_of_wheels = num_of_wheels || 2;
+ this.speed = speed || 20;
+}
+Bycicle.prototype = Object.create(Car.prototype);
+Bycicle.prototype.drive = function () {
+ console.log('Driving on bycicle' + this.speed + 'kms per hour');
+}
+
+var car = new Car(4, 40);
+var truck = new Truck(6);
+var bycicle = new Bycicle(2, 20);
+
+/*
 function closure(num_of_wheels, speed) {
     Car.call(this, num_of_wheels, speed);
     return {
@@ -23,3 +44,4 @@ function closure(num_of_wheels, speed) {
 }
 
 var newCar = closure();
+*/
