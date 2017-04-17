9b6b3898f7c66ebe738a0e03da6c6dbce7413303
martofsky <martofsky.r.j@gmail.com>
2017-04-17 21:07:08 +0300

    upd

--- homework/js/cars.js
+++ homework/js/cars.js
@@ -1,47 +1,32 @@
-var Car = function(num_of_wheels, speed) {
-    this.num_of_wheels = num_of_wheels || '--';
-    this.speed = speed || '--';
-}
-Car.prototype.drive = function() {
-        return console.log('Driving on car' + this.speed + 'kms per hour');
-}
+var Cars = (function() { //namespace
+    var Car = function(wheels, speed) {
+        var privateWheels = wheels || '--';
+        var privateSpeed = speed || '--';
 
-var Truck = function(num_of_wheels, speed) { 
- Car.call(this, num_of_wheels, speed); 
-} 
-Truck.prototype = Object.create(Car.prototype);
-Truck.prototype.drive = function () {
- console.log('Driving on truck' + this.speed + 'kms per hour');
-}
-
-var Bycicle = function(num_of_wheels, speed) { 
- Car.call(this, num_of_wheels, speed); 
- this.num_of_wheels = num_of_wheels || 2;
- this.speed = speed || 20;
-}
-Bycicle.prototype = Object.create(Car.prototype);
-Bycicle.prototype.drive = function () {
- console.log('Driving on bycicle' + this.speed + 'kms per hour');
-}
-
-var car = new Car(4, 40);
-var truck = new Truck(6);
-var bycicle = new Bycicle(2, 20);
-
-/*
-function closure(num_of_wheels, speed) {
-    Car.call(this, num_of_wheels, speed);
-    return {
-        get: function(num_of_wheels, speed) {
-            return JSON.parse(JSON.stringify(num_of_wheels, speed));
-        },
-        set: function(newNum_of_wheels, newSpeed) {
-            if (typeof (newNum_of_wheels, newSpeed) === "object") {
-                value = JSON.parse(JSON.stringify(newNum_of_wheels, newSpeed));
-            }
+        this.getWheels = function() {
+            return  privateWheels;
+        }
+        this.getSpeed = function() {
+            return privateSpeed;
         }
     }
-}
+    Car.prototype.drive = function() {
+            return console.log('Driving on car' + this.getSpeed + 'kms per hour');
+    }
+    var Truck = function(speed) { 
+    Car.call(this, 6, speed); 
+    } 
+    Truck.prototype = Object.create(Car.prototype);
+    var Bycicle = function() { 
+    Car.call(this, 2, 20); 
+    }
+    Bycicle.prototype = Object.create(Car.prototype);
+    return {
+        Truck: Truck,
+        Bycicle: Bycicle
+    }
+})();
 
-var newCar = closure();
-*/
+    var car = new Car(4, 40);
+    var truck = new Truck(6);
+    var bycicle = new Bycicle(2, 20);
