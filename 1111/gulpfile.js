32e5f08c2c426fd1428d6c2f7e72e0060640d82b
martofsky <martofsky.r.j@gmail.com>
2017-05-22 19:33:17 +0300

    upd

---
+++ 1111/gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),    
+    bower = require('gulp-bower'),    
+    bs = require('browser-sync');
+
+var config = {
+    sassPath: './resources/sass',
+    bowerDir: './bower_components'
+}
+
+gulp.task('bower', function() {
+    return bower()
+        .pipe(gulp.dest(config.bowerDir))
+});
+
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+        .pipe(gulp.dest('./public/fonts'));
+});
+
+gulp.task('dev', function() {
+    bs.init({
+        server: {
+            baseDir: "./public"
+        },
+        open: false,
+        reloadDelay: 1000,
+        reloadDebounce: 1000
+    });
+
+    gulp.watch("resources/sass/*.scss", ['sass']);
+    gulp.watch("public/*.html").on('change', bs.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("resources/sass/*.scss")
+        .pipe(sass({
+            style: 'compressed',
+            loadPath: [
+                './resources/sass',
+                config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+                config.bowerDir + '/fontawesome/scss',
+            ]
+        }))
+        .pipe(gulp.dest("./public/css"))
+        .pipe(bs.stream());
+});
+
+gulp.task('default', ['bower', 'icons', 'sass', 'dev']);
