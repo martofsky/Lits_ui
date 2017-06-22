a4f6d663fadaa4d9129282bd7fe0a7403b06d9da
martofsky <martofsky.r.j@gmail.com>
2017-06-22 17:23:56 +0300

    e

--- Flight/js/parse-info.js
+++ Flight/js/parse-info.js
@@ -1,5 +1,5 @@
 var xmlhttp = new XMLHttpRequest();
-var url = "request/info.txt";
+var url = "./request/info.txt";
 
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
