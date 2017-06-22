d2433381df6303db96292b6a5ed2d14df43f8543
martofsky <martofsky.r.j@gmail.com>
2017-06-22 17:15:55 +0300

    flight

---
+++ Flight/js/bubble.js
@@ -0,0 +1,54 @@
+//balloon
+document.getElementById('area1').addEventListener("click", () => {
+  var bubble = document.getElementById('bubble1');
+  if (bubble.style.display === 'none') {
+    bubble.style.display = 'block';
+  } else {
+    bubble.style.display = 'none';
+  }
+});
+
+document.getElementById('area2').addEventListener("click", () => {
+  var bubble = document.getElementById('bubble2');
+  if (bubble.style.display === 'none') {
+    bubble.style.display = 'block';
+  } else {
+    bubble.style.display = 'none';
+  }
+});
+
+document.getElementById('area3').addEventListener("click", () => {
+  var bubble = document.getElementById('bubble3');
+  if (bubble.style.display === 'none') {
+    bubble.style.display = 'block';
+  } else {
+    bubble.style.display = 'none';
+  }
+});
+
+document.getElementById('area4').addEventListener("click", () => {
+  var bubble = document.getElementById('bubble4');
+  if (bubble.style.display === 'none') {
+    bubble.style.display = 'block';
+  } else {
+    bubble.style.display = 'none';
+  }
+});
+
+document.getElementById('area5').addEventListener("click", () => {
+  var bubble = document.getElementById('bubble5');
+  if (bubble.style.display === 'none') {
+    bubble.style.display = 'block';
+  } else {
+    bubble.style.display = 'none';
+  }
+});
+
+document.getElementById('area6').addEventListener("click", () => {
+  var bubble = document.getElementById('bubble6');
+  if (bubble.style.display === 'none') {
+    bubble.style.display = 'block';
+  } else {
+    bubble.style.display = 'none';
+  }
+});
